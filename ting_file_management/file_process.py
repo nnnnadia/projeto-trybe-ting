7afe6f39973d4fbb5e4ef478bebd689a519fbccf
nnnnadia <tristaonadia@gmail.com>
2023-07-09 22:32:17 -0300

    feat: implements `process` function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file: str, instance: Queue):
+    if instance.was_filed(path_file):
+        return
+
+    file = txt_importer(path_file)
+    if file:
+        formatted_file_obj = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file,
+        }
+        instance.enqueue(formatted_file_obj)
+        instance.add_file_to_set(formatted_file_obj)
+        print(formatted_file_obj)
 
 
 def remove(instance):
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,20 +3,28 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self.items = list()
+        self.files = list()
+        self.files_filed = set()
 
     def __len__(self):
-        return len(self.items)
+        return len(self.files)
 
     def enqueue(self, value):
-        self.items.append(value)
+        self.files.append(value)
 
     def dequeue(self):
-        if len(self.items) == 0:
+        if len(self.files) == 0:
             return None
-        return self.items.pop(0)
+        return self.files.pop(0)
 
     def search(self, index):
-        if index < 0 or index >= len(self.items):
+        if index < 0 or index >= len(self.files):
             raise IndexError("Índice Inválido ou Inexistente")
-        return self.items[index]
+        return self.files[index]
+
+    def was_filed(self, file):
+        return file in self.files_filed
+
+    def add_file_to_set(self, file):
+        file_name = file["nome_do_arquivo"]
+        self.files_filed.add(file_name)
