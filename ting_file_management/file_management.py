8045a52f48644ceafbe3a72047df965f73d96a15
nnnnadia <tristaonadia@gmail.com>
2023-07-09 21:14:28 -0300

    feat: implements `txt_importer` function

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido")
+    try:
+        with open(path_file, "r") as file:
+            data = file.read()
+        return data.split("\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
